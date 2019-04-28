3fd14efb611a465c9081b719987fb4017ec7e7be
karetsu <myogibo@gmail.com>
2019-04-28 22:00:00 +0100

    feat: MC birthday party

--- src/Chapter002/Probability.hs
+++ src/Chapter002/Probability.hs
@@ -5,6 +5,10 @@ module Chapter002.Probability where
 -- we will mostly be doing Monte Carlo simulations for this module
 import System.Random
 
+import Data.List (nub)
+import Data.List.Split (chunksOf)
+
+
 -- Dice even sum ----------------------------------------------------------------
 -- roll two dice, what's the probability that the sum of the values is even?
 
@@ -37,4 +41,32 @@ rollTwo = do
   evenDiceMC (10^6) 6
 
 -- Partially matching passwords -------------------------------------------------
+password :: String
+password = "3xyZu4vN"
+-- TODO
 
+
+-- The birthday problem ---------------------------------------------------------
+matchExists :: Int -> Float
+matchExists n = 1 - product fractions
+  where
+    fractions = [fromIntegral k / 365 | k <- [365,364..(365-n+1)]]
+
+matchExists' :: Int -> Float
+matchExists' n = 1 - factorial
+  where
+    factorial = fromIntegral (product [365,364..(365-n+1)]) / (365^n)
+
+
+matchedSim :: Ord a => [a] -> Int
+matchedSim xs = if xs == nub xs then 0 else 1
+
+
+matchExistsMC :: Int  -- number of people
+              -> Int  -- number of simulations
+              -> IO Double
+matchExistsMC n s = do
+  g <- newStdGen
+  let days = chunksOf n (take (n*s) (randomRs (1,365) g)::[Int])
+  let count = (sum . map matchedSim) days
+  return $ fromIntegral count / fromIntegral s
